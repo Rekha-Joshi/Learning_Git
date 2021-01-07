bb61c5087e330e5f4a7e7110fd05217100bbe5b5
Rekha Joshi <rekha.joshi2@gmail.com>
2021-01-07 12:32:27 -0800

    testing the staging and not staging

--- src/test/java/com/qa/automation/Sanity.java
+++ src/test/java/com/qa/automation/Sanity.java
@@ -3,5 +3,6 @@ package com.qa.automation;
 public class Sanity {
 public Sanity() {
 	System.out.println("Automating sanity test here");
+	System.out.println("checking if changin a file shows any different message in git");
 }
 }
