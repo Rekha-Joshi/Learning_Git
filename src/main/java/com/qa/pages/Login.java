3fa8d879c8125e791109f4ec8c2886e297dca6a4
Rekha Joshi <rekha.joshi2@gmail.com>
2021-01-07 11:56:16 -0800

    My first commit in this project

---
+++ src/main/java/com/qa/pages/Login.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Login {
+	
+	public void username() {
+		System.out.println("All login related pages will be here");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/Login.java
@@ -0,0 +1,16 @@
+package com.qa.tests;
+
+import org.junit.Test;
+
+public class Login {
+	
+	@Test
+	public void username() {
+		System.out.println("Test for valid username ");
+	}
+	
+	public void pass() {
+		System.out.println("Test for valid password");
+	}
+
+}
